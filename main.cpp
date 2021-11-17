8001a4cf3f402c7833f68000050804d6ae6fdf83
juan <juanrm071299@gmail.com>
2021-11-17 17:37:22 -0600

    setting project

--- main.cpp
+++ main.cpp
@@ -1,21 +1,11 @@
-#include <QCoreApplication>
-#include <QDebug>
+#include "window.h"
+
 #include <QApplication>
-#include <QPushButton>
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    qDebug() << "Hello World";
-    return QApplication::exec();
-    /*
-    QCoreApplication a(argc, argv);
-    qDebug() << "Hello World";
-    return QCoreApplication::exec();
-     */
+    window w;
+    w.show();
+    return a.exec();
 }
-
-
