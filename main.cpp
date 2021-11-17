97db26c218bd1e830f98f6891b2a4b95259ee4e3
juan <juanrm071299@gmail.com>
2021-11-17 13:01:17 -0600

    qt widgets

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[]) {
+
+    QApplication a(argc, argv);
+    QPushButton button("Hello world!", nullptr);
+    button.resize(200, 100);
+    button.show();
+    qDebug() << "Hello World";
+    return QApplication::exec();
+    /*
     QCoreApplication a(argc, argv);
     qDebug() << "Hello World";
     return QCoreApplication::exec();
+     */
 }
+
+
