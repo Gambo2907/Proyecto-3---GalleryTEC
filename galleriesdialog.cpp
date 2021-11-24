4c7ab990bc0a226a1181d51974d6189d0a9950f9
juan <juanrm071299@gmail.com>
2021-11-23 22:54:11 -0600

    list modificado

--- galleriesdialog.cpp
+++ galleriesdialog.cpp
@@ -27,3 +27,25 @@ void GalleriesDialog::on_pushButton_Gback_clicked()
     hide();
     parentWidget()->show();
 }
+
+
+//#include "galleriesdialog.h"
+//#include "ui_galleriesdialog.h"
+//#include <QDir>
+//GalleriesDialog::GalleriesDialog(QWidget *parent) :
+//        QDialog(parent),
+//        ui(new Ui::GalleriesDialog)
+//{
+//
+//    ui->setupUi(this);
+//    //QListWidgetItem *item = new QListWidgetItem(QIcon(":rec/imgs/carpeta.png"),"Carpeta");
+//    //ui->listWidget_galleries->addItem(item);
+//    QDir dir (":/Proyecto-3-GalleryTEC/imgs");
+//            foreach(QFileInfo var, dir.drives()){
+//            ui->listWidget_galleries->addItem(dir.dirName());
+//
+//        }
+//
+//}
+//
+//GalleriesDialog::~GalleriesDialog()
--- imagesdialog.cpp
+++ imagesdialog.cpp
@@ -27,3 +27,24 @@ void ImagesDialog::on_pushButton_Iback_clicked()
     hide();
     parentWidget()->show();
 }
+
+
+
+//#include "imagesdialog.h"
+//#include "ui_imagesdialog.h"
+//#include <QDir>
+//ImagesDialog::ImagesDialog(QWidget *parent) :
+//        QDialog(parent),
+//        ui(new Ui::ImagesDialog)
+//{
+//    ui->setupUi(this);
+//    QDir dir (":/GitHub/Proyecto-3-GalleryTEC/imgs");
+//            foreach(QFileInfo var, dir.entryInfoList()){
+//            if(var.isFile()) {
+//                ui->listWidget_images->addItem(var.fileName());
+//            }
+//
+//        }
+//}
+//
+//ImagesDialog::~ImagesDialog()
