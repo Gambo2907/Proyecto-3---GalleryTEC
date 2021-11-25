66ef2690fcc772c1ccfadd591450cdb1e542bc10
gambo2907 <sgamo2220@gmail.com>
2021-11-24 20:14:08 -0600

    resource

--- registerdialog.cpp
+++ registerdialog.cpp
@@ -8,8 +8,8 @@ RegisterDialog::RegisterDialog(QWidget *parent) :
     ui(new Ui::RegisterDialog)
 {
     ui->setupUi(this);
-    QPixmap pixImage("/home/juanda/CLionProjects/Proyecto-3-GalleryTEC/imgs/image1.jpg");
-    QPixmap pixFolder("/home/juanda/CLionProjects/Proyecto-3-GalleryTEC/imgs/carpeta1.jpg");
+    QPixmap pixImage(":/imgs/imgs/image1.jpg");
+    QPixmap pixFolder(":/imgs/imgs/carpeta1.jpg");
     ui->label_pictures->setPixmap(pixImage.scaled(121,121,Qt::KeepAspectRatio));
     ui->label_folder->setPixmap(pixFolder.scaled(121,121,Qt::KeepAspectRatio));
 
